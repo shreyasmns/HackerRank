de2142e968a774a2601d872290330bacd55c50c6
Shreyas MN <shreyaschinni@gmail.com>
2016-05-23 23:25:49 -0400

    Create Inheritance1.java

---
+++ Java/OOPS/Inheritance1.java
@@ -0,0 +1,40 @@
+package oops;
+
+public class Inheritance1 {
+
+	public static void main(String[] args) {
+		
+		class Animal{
+			   void walk()
+			   {
+			      System.out.println("I am walking");
+			   }
+			}
+
+		class Bird extends Animal
+			{
+			   void fly()
+			   {
+			      System.out.println("I am flying");
+			   }
+			    
+			   void sing(){
+			        System.out.println("I am singing");
+			    }
+			}
+		
+		class Solution
+			{
+
+			   public void main(String args[])
+			   {
+
+			     Bird bird = new Bird();
+			     bird.walk();
+			     bird.fly();
+			     bird.sing();
+			   
+			   }
+			}
+     	}
+    }
